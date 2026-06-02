64ade2470b4885821d2edd5ffe2083c1c81e45cb
danielhuata2-ship-it <danielhuata2@gmail.com>
2026-06-02 13:25:37 -0400

    implementacion del template, a mejorar

--- frontend_tienda_de_accesorios_y_productos_de_belleza/src/App.vue
+++ frontend_tienda_de_accesorios_y_productos_de_belleza/src/App.vue
@@ -1,95 +1,36 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { useAuthStore } from './stores/index.ts';
+import { RouterView } from 'vue-router'
+import '@/assets/js/jquery-3.3.1.min.js'
+import '@/assets/js/bootstrap.min.js'
+import '@/assets/js/jquery.nicescroll.min.js'
+import '@/assets/js/jquery.magnific-popup.min.js'
+import '@/assets/js/jquery.countdown.min.js'
+import '@/assets/js/jquery.slicknav.js'
+import '@/assets/js/mixitup.min.js'
+import '@/assets/js/owl.carousel.min.js'
+import '@/assets/js/main.js'
 
-const authStore = useAuthStore();
+import MainHeader from '@/components/MainHeader.vue'
+import MainFooter from '@/components/MainFooter.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main>
+    <MainHeader />
 
-    <div class="wrapper">
-      <HelloWorld :msg="'Tienda de Accesorios y Productos de Belleza' + (authStore.empleado ? ' - Hola, ' + authStore.empleado : '')"
-/>
- <
-      <nav>
-        <RouterLink to="/">Inicio</RouterLink>
-        <RouterLink v-if="!authStore.token" to="login">Iniciar Sesión</RouterLink>
-        <span v-else>
-        <RouterLink to="/productos">Productos</RouterLink>
-        <RouterLink to="/clientes">Clientes</RouterLink>
-        <RouterLink to="/proveedores">Proveedores</RouterLink>
-        <a @click="authStore.logout()" class="text-danger">Salir</a>
-        </span>
-      </nav>
-    </div>
-  </header>
+    <RouterView />
+  </main>
 
-  <RouterView />
+  <MainFooter />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+<style>
+@import '@/assets/css/bootstrap.min.css';
+@import '@/assets/css/font-awesome.min.css';
+@import '@/assets/css/elegant-icons.css';
+@import '@/assets/css/magnific-popup.css';
+@import '@/assets/css/nice-select.css';
+@import '@/assets/css/owl.carousel.min.css';
+@import '@/assets/css/slicknav.min.css';
+@import '@/assets/css/style.css';
 </style>
---
+++ frontend_tienda_de_accesorios_y_productos_de_belleza/src/components/MainHeader.vue
@@ -0,0 +1,126 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/index.ts'
+import { RouterLink } from 'vue-router'
+
+const authStore = useAuthStore()
+</script>
+
+<template>
+  <div>
+    <div class="offcanvas-menu-overlay"></div>
+    <div class="offcanvas-menu-wrapper">
+        <div class="offcanvas__option">
+            <div class="offcanvas__links">
+                <a href="#">Sign in</a>
+                <a href="#">FAQs</a>
+            </div>
+            <div class="offcanvas__top__hover">
+                <span>Usd <i class="arrow_carrot-down"></i></span>
+                <ul>
+                    <li>USD</li>
+                    <li>EUR</li>
+                    <li>USD</li>
+                </ul>
+            </div>
+        </div>
+        <div class="offcanvas__nav__option">
+            <a href="#" class="search-switch"><img src="img/icon/search.png" alt=""></a>
+            <a href="#"><img src="img/icon/heart.png" alt=""></a>
+            <a href="#"><img src="img/icon/cart.png" alt=""> <span>0</span></a>
+            <div class="price">$0.00</div>
+        </div>
+        <div id="mobile-menu-wrap"></div>
+        <div class="offcanvas__text">
+            <p>Free shipping, 30-day return or refund guarantee.</p>
+        </div>
+    </div>
+    <header class="header">
+      <div class="header__top">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 col-md-7">
+              <div class="header__top__left">
+                <p>Free shipping, 30-day return or refund guarantee.</p>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-5">
+              <div class="header__top__right">
+                <div class="header__top__links">
+                  <a href="#">Sign in</a>
+                  <a href="#">FAQs</a>
+                </div>
+                <div class="header__top__hover">
+                  <span>Usd <i class="arrow_carrot-down"></i></span>
+                  <ul>
+                    <li>USD</li>
+                    <li>EUR</li>
+                    <li>USD</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-3">
+            <div class="header__logo">
+              <a href="./index.html"><img src="img/logo.png" alt="" /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6">
+            <nav class="header__menu mobile-menu">
+              <ul>
+                <li class="active">
+                  <RouterLink to="/">Inicio</RouterLink>
+                </li>
+
+                <li>
+                  <RouterLink to="/productos">Productos</RouterLink>
+                </li>
+
+                <li>
+                  <RouterLink to="/clientes">Clientes</RouterLink>
+                </li>
+
+                <li>
+                  <RouterLink to="/proveedores">Proveedores</RouterLink>
+                </li>
+
+                <li>
+                  <RouterLink to="/about">Acerca de</RouterLink>
+                </li>
+
+                <li v-if="!authStore.token">
+                  <RouterLink to="/login">Iniciar Sesión</RouterLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div class="col-lg-3 col-md-3">
+            <div class="header__nav__option">
+              <a href="#" class="search-switch"><img src="img/icon/search.png" alt="" /></a>
+              <a href="#"><img src="img/icon/heart.png" alt="" /></a>
+              <a href="#"><img src="img/icon/cart.png" alt="" /> <span>0</span></a>
+              <div class="price">$0.00</div>
+            </div>
+          </div>
+        </div>
+        <div class="canvas__open"><i class="fa fa-bars"></i></div>
+      </div>
+    </header>
+    <!-- Search Begin -->
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch">+</div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
+        </div>
+    </div>
+    <!-- Search End -->
+  </div>
+</template>
+
+<style scoped></style>
